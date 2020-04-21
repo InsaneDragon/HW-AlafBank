d1b29217474b65ab75d1505c7a4caea8590b9c2e
C#Developer <shahzodkiller11@gmail.com>
2020-04-21 19:33:47 +0500

    creditAded

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AlafBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class credit
+    {
+        public string Gender { get; set; }
+        public string FamilyStatus { get; set; }
+        public string Age { get; set; }
+        public string Nationality { get; set; }
+        public double Salary { get; set; }
+        public string ClosedCredits { get; set; }
+        public string ExpiredCredits { get; set; }
+        public string TargetCredit { get; set; }
+        public string CredidTerm { get; set; }
+        public double Sum { get; set; }
+    }
+}
